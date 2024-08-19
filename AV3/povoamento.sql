90d87d2d8f397fd566c9ecbfd523be0c4c54669f
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-08-18 22:49:47 -0300

    Create povoamento.sql

---
+++ AV3/povoamento.sql
@@ -0,0 +1,54 @@
+ALTER SESSION
+SET
+    NLS_DATE_FORMAT = 'DD-MM-YYYY';
+
+-- Usuario
+INSERT INTO
+    usuario (email, nome, sobrenome, data_nasc)
+VALUES
+    ('ram3@cin.ufpe.br', 'Rebeca','Menezes', '28-11-2003');
+
+INSERT INTO
+    usuario (email, nome, sobrenome, data_nasc)
+VALUES
+    ('lblp@cin.ufpe.br', 'Letícia', 'Pedrosa', '17-09-2002');
+
+INSERT INTO
+    usuario (email, nome, sobrenome, data_nasc)
+VALUES
+    ('pms5@cin.ufpe.br', 'Pedro','Martins', '30-04-2002');
+
+INSERT INTO
+    usuario (email, nome, sobrenome, data_nasc)
+VALUES
+    ('jade.oliveira@gmail.com', 'Jade','Oliveira', '22-08-1995');
+
+INSERT INTO
+    usuario (email, nome, sobrenome, data_nasc)
+VALUES
+    ('rosa.pires@gmail.com', 'Rosa','Pires', '20-09-1998');
+
+INSERT INTO
+    usuario (email, nome, sobrenome, data_nasc)
+VALUES
+    ('mlmn3@cin.ufpe.br', 'maria leticia','maranhao', '17-06-2003');
+
+INSERT INTO
+    usuario (email, nome, sobrenome, data_nasc)
+VALUES
+    ('gmsmr@cin.ufpe.br', 'guilherme', 'maranhao', '02-03-2004');
+
+INSERT INTO
+    usuario (email, nome, sobrenome, data_nasc)
+VALUES
+    ('mcta@cin.ufpe.br', 'maria clara', 'albuquerque', '23-08-2002');
+
+INSERT INTO
+    usuario (email, nome, sobrenome, data_nasc)
+VALUES
+    ('jrco@cin.ufpe.br', 'joao victor', 'omena', '05-11-2004');
+
+INSERT INTO
+    usuario (email, nome, sobrenome, data_nasc)
+VALUES
+    ('cristaldog@gmail.com', 'cristal', 'cachorra', '06-07-2010');
