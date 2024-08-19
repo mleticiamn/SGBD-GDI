44de9ff97ab4c640b78c65e09a2946b7dab1e818
Maria Letícia <mlmn3@cin.ufpe.br>
2024-08-18 23:36:50 -0300

    povoamento de telefones

--- AV3/povoamento.sql
+++ AV3/povoamento.sql
@@ -67,3 +67,70 @@ INSERT INTO
     usuario (email, nome, sobrenome, data_nasc)
 VALUES
     ('cristaldog@gmail.com', 'cristal', 'cachorra', '06-07-2010');
+
+
+-- TELEFONES
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('ram3@cin.ufpe.br', '23-098761234');
+
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('lblp@cin.ufpe.br', '67-231435465');
+
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('pms5@cin.ufpe.br', '81-987123531');
+
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('jane.austen@gmail.com', '29-483718293');
+
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('rick.riordan@gmail.com', '28-318373928');
+    
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('john.green@gmail.com', '21-829574625');
+    
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('machado.de.assis@gmail.com', '21-382817293');
+    
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('paula.pimenta@gmail.com', '11-383726384');
+
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('mlmn3@cin.ufpe.br','81-998124438');
+
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('gmsmr@cin.ufpe.br', '81-918374641');
+
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('mcta@cin.ufpe.br', '81-976153812');
+
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('jrco@cin.ufpe.br', '82-978432451');
+
+INSERT INTO
+    telefones (email, telefone)
+VALUES
+    ('cristaldog@gmail.com', '83-178123451');
