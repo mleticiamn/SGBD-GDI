b41bf75af852f1d12d51722a431d2333e54b20ab
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-08-20 23:27:42 -0300

    Update consultas_sql.sql

--- AV4/consultas_sql.sql
+++ AV4/consultas_sql.sql
@@ -1,5 +1,10 @@
--- Insere novo telefone para Rebeca
+-- Insere novo telefone para Rebeca Menezes
 INSERT INTO
     telefones (email, telefone)
 VALUES
     ('ram3@cin.ufpe.br', '23-088769235');
+
+-- Atualiza a data de nascimento de Pedro Martins
+UPDATE usuario
+SET data_nasc = '30-04-2003'
+WHERE email = 'pms5@cin.ufpe.br';
