82b9f602440c35ba877a490a90e39d4ba04c77ae
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-08-21 22:02:44 -0300

    Update consultas_pl.sql to delete a query and insert another

--- AV4/consultas_pl.sql
+++ AV4/consultas_pl.sql
@@ -1,21 +1,41 @@
-/* Exibe todos os usuários da plataforma que nasceram antes do ano 2000,
-incluindo seus respectivos emails, nomes, sobrenomes e datas de nascimento */
+/* Calcula e exibe a quantidade de seguidores de cada usuário na plataforma, exibindo também a média de seguidores por usuário. 
+O resultado inclui o email, o nome completo e a quantidade de seguidores de cada usuário. */
 DECLARE
-    CURSOR usuario_cursor IS
-        SELECT u.email, u.nome, u.sobrenome, u.data_nasc FROM usuario u
-        WHERE u.data_nasc < TO_DATE('01-01-2000', 'DD-MM-YYYY');
-        
-    v_email       usuario.email%TYPE;
-    v_nome        usuario.nome%TYPE;
-    v_sobrenome   usuario.sobrenome%TYPE;
-    v_data_nasc   usuario.data_nasc%TYPE;
+    v_email usuario.email%TYPE;
+    v_nome usuario.nome%TYPE;
+    v_sobrenome usuario.sobrenome%TYPE;
+    v_qtd_seguidores NUMBER;
+    v_media_seguidores NUMBER;
+
+    CURSOR c_seguidores IS
+        SELECT u.email, u.nome, u.sobrenome, COUNT(s.seguidor) AS qtd_seguidores FROM usuario u
+        LEFT JOIN seguir s ON u.email = s.seguido
+        GROUP BY u.email, u.nome, u.sobrenome;
+    
+    v_soma_total NUMBER := 0;
+    v_contador NUMBER := 0;
 
 BEGIN
-    OPEN usuario_cursor;
+    OPEN c_seguidores;
+    
     LOOP
-        FETCH usuario_cursor INTO v_email, v_nome, v_sobrenome, v_data_nasc;
-        EXIT WHEN usuario_cursor%NOTFOUND;
-        DBMS_OUTPUT.PUT_LINE('Email: ' || v_email || ', Nome: ' || v_nome || ' ' || v_sobrenome || ', Data de Nascimento: ' || TO_CHAR(v_data_nasc, 'DD-MM-YYYY'));
+        FETCH c_seguidores INTO v_email, v_nome, v_sobrenome, v_qtd_seguidores;
+        EXIT WHEN c_seguidores%NOTFOUND;
+        
+        DBMS_OUTPUT.PUT_LINE('Email: ' || v_email || ', Usuário(a): ' || v_nome || ' ' || v_sobrenome || ', Quantidade de Seguidores: ' || v_qtd_seguidores);
+        
+        v_soma_total := v_soma_total + v_qtd_seguidores;
+        v_contador := v_contador + 1;
     END LOOP;
-    CLOSE usuario_cursor;
+    
+    CLOSE c_seguidores;
+
+    IF v_contador > 0 THEN
+        v_media_seguidores := v_soma_total / v_contador;
+    ELSE
+        v_media_seguidores := 0;
+    END IF;
+    
+    DBMS_OUTPUT.PUT_LINE('Média de Seguidores por Usuário: ' || v_media_seguidores);
 END;
+/
