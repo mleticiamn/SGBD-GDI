99f44d2dfadcbcc7deb1bcf1d13a8a480bb8fdf5
Maria Letícia <mlmn3@cin.ufpe.br>
2024-09-16 20:58:51 -0300

    feat: adding alter type

--- AV5/criacao_tipos.sql
+++ AV5/criacao_tipos.sql
@@ -177,7 +177,7 @@ CREATE OR REPLACE TYPE BODY tp_obra AS
             RETURN 1;
         ELSE
             RETURN 0;
-        ELSE IF;
+        END IF;
     END;
 END;
 /
@@ -223,7 +223,10 @@ CREATE OR REPLACE TYPE tp_seguir AS OBJECT(
 -- TIPO CURTIR
 CREATE OR REPLACE TYPE tp_curtir AS OBJECT(
     email_curte REF tp_usuario,
-    email_publicou REF tp_usuario,
-    cod_pub REF tp_publicacao
+    email_publicou REF tp_usuario
 );
+/
+
+ALTER TYPE tp_curtir
+ADD ATRIBUTE (cod_pub REF tp_publicacao);
 /
