33653ce3b575404d842c9a67a4f4a720a24020c6
mleticiamn <mlmn3@cin.ufpe.br>
2024-09-16 12:00:58 -0300

    feat: inserting values into tb_autor

--- AV5/povoamento_or.sql
+++ AV5/povoamento_or.sql
@@ -11,7 +11,7 @@ INSERT INTO tb_usuario VALUES(
             'Maranhao'
         ),
         '17-06-2003',
-        va_tp_telefones(tp_telefones('819000000'), tp_telefones('32280000'))
+        va_tp_telefones(tp_telefones('81900000000'), tp_telefones('81932280000'))
     )
 );
 /
@@ -24,7 +24,7 @@ INSERT INTO tb_usuario VALUES(
             'da Silva'
         ),
         '21-09-1998',
-        va_tp_telefones(tp_telefones('819111111'), tp_telefones('32281111'))
+        va_tp_telefones(tp_telefones('81911111111'), tp_telefones('81932281111'), tp_telefones('81932282222'))
     )
 );
 /
@@ -37,7 +37,7 @@ INSERT INTO tb_usuario VALUES(
             'Santos'
         ),
         '05-03-2000',
-        va_tp_telefones(tp_telefones('819222222'), tp_telefones('32282222'))
+        va_tp_telefones(tp_telefones('81922222222'))
     )
 );
 /
@@ -50,7 +50,62 @@ INSERT INTO tb_usuario VALUES(
             'Costa'
         ),
         '12-12-2001',
-        va_tp_telefones(tp_telefones('819333333'), tp_telefones('32283333'))
+        va_tp_telefones(tp_telefones('81933333333'), tp_telefones('81932283333'))
+    )
+);
+/
+
+-- INSERINDO AUTORES
+INSERT INTO tb_autor VALUES(
+    tp_autor(
+        'rick.riordan@gmail.com',
+        tp_nome_completo(
+            'Rick',
+            'Riordan'
+        ),
+        '05-06-1964',
+        va_tp_telefones(tp_telefones('81944444444')),
+        'Richard Russell Riordan Jr. é um escritor, mais conhecido por escrever a série Percy Jackson e Os Olimpianos',
+        tp_nt_premios(
+            tp_premios('Hampshire Book Award'),
+            tp_premios('Shamus Award for Best First P. I. Novel'),
+            tp_premios('Prêmio Edgar: Melhor Brochura Original')
+        )
+    )
+);
+/
+
+INSERT INTO tb_autor VALUES(
+    tp_autor(
+        'jk.rowling@gmail.com',
+        tp_nome_completo(
+            'J.K.',
+            'Rowling'
+        ),
+        '31-07-1965',
+        va_tp_telefones(tp_telefones('81955555555'), tp_telefones('81932283333'), tp_telefones('81932284444')),
+        'Joanne Rowling, conhecida por seu pseudônimo J.K. Rowling, é uma escritora britânica, autora da série Harry Potter',
+        tp_nt_premios(
+            tp_premios('Prêmio Bram Stoker')
+        )
+    )
+);
+/
+
+INSERT INTO tb_autor VALUES(
+    tp_autor(
+        'george.martin@gmail.com',
+        tp_nome_completo(
+            'George',
+            'R.R. Martin'
+        ),
+        '20-09-1948',
+        va_tp_telefones(tp_telefones('81966666666')),
+        'George Raymond Richard Martin, conhecido como George R.R. Martin, é um escritor e roteirista norte-americano, autor de As Crônicas de Gelo e Fogo',
+        tp_nt_premios(
+            tp_premios('Hugo Award for Best Novella'),
+            tp_premios('Nebula Award for Best Novella')
+        )
     )
 );
 /
