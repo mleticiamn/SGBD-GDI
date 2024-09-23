0404e775ad47bd918429027cde6cd24d74a90c97
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-09-23 11:06:55 -0300

    feat: add query related to tb_usuario_pub

--- AV6/consultas_or.sql
+++ AV6/consultas_or.sql
@@ -7,8 +7,8 @@ WHERE LENGTH(U.email) > 20;
 SELECT A.nome_completo.nome AS nome, A.nome_completo.sobrenome AS sobrenome
 FROM tb_autor A
 WHERE CARDINALITY(A.lista_premios) = (
-    SELECT MAX(CARDINALITY(B.lista_premios))
-    FROM tb_autor B
+	SELECT MAX(CARDINALITY(B.lista_premios))
+	FROM tb_autor B
 );
 
 -- Seleciona as publicações feitas em março de 2024 e exibe todos os atributos associados a essas publicações
@@ -16,3 +16,9 @@ SELECT DEREF(P.email).email AS email, P.cod_pub, P.conteudo, P.data_pub
 FROM tb_publicacao P
 WHERE P.data_pub >= TO_DATE('2024-03-01', 'YYYY-MM-DD')
 	AND P.data_pub < TO_DATE('2024-04-01', 'YYYY-MM-DD');
+
+-- Seleciona os usuários que realizaram publicações e exibe seus nomes em ordem alfabética, eliminando resultados duplicados
+SELECT DISTINCT U.nome_completo.nome AS nome
+FROM tb_usuario_pub T, tb_usuario U
+WHERE u.email = DEREF(T.email).email
+ORDER BY U.nome_completo.nome;
