2e534c362325c2d845830cf2b7300099e9d44d39
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-09-23 10:10:51 -0300

    feat: add query

--- AV6/consultas_or.sql
+++ AV6/consultas_or.sql
@@ -3,3 +3,10 @@ SELECT U.email
 FROM tb_usuario U
 WHERE LENGTH(U.email) > 20;
 
+-- Descobre qual autor registrado tem mais prêmios e printa o seu nome completo
+SELECT A.nome_completo.nome AS nome, A.nome_completo.sobrenome AS sobrenome
+FROM tb_autor A
+WHERE CARDINALITY(A.lista_premios) = (
+    SELECT MAX(CARDINALITY(B.lista_premios))
+    FROM tb_autor B
+);
