1b895b53827ea023a0b9418881c1e2a516f63150
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-09-23 22:58:32 -0300

    Update povoamento_or.sql

--- AV5/povoamento_or.sql
+++ AV5/povoamento_or.sql
@@ -67,6 +67,45 @@ INSERT INTO tb_usuario VALUES(
     )
 );
 /
+
+INSERT INTO tb_usuario VALUES( 
+    tp_usuario( 
+        'rick.riordan@gmail.com', 
+        tp_nome_completo( 
+            'Rick', 
+            'Riordan' 
+        ), 
+        '05-06-1964', 
+        va_tp_telefones(tp_telefones('81944444444')) 
+    ) 
+);
+/
+
+INSERT INTO tb_usuario VALUES( 
+    tp_usuario( 
+        'jk.rowling@gmail.com', 
+        tp_nome_completo( 
+            'J.K.', 
+            'Rowling' 
+        ), 
+        '31-07-1965', 
+        va_tp_telefones(tp_telefones('81955555555'), tp_telefones('81932283333'), tp_telefones('81932284444')) 
+    ) 
+);
+/
+
+INSERT INTO tb_usuario VALUES( 
+    tp_usuario( 
+        'george.martin@gmail.com', 
+        tp_nome_completo( 
+            'George', 
+            'R.R. Martin' 
+        ), 
+        '20-09-1948', 
+        va_tp_telefones(tp_telefones('81966666666')) 
+    ) 
+);
+/
     
 -- INSERINDO NA TABELA AUTOR
 INSERT INTO tb_autor VALUES(
@@ -124,7 +163,10 @@ INSERT INTO tb_autor VALUES(
 /
 
 -- CRIANDO SEQUẼNCIA PARA COD_PUB
-CREATE SEQUENCE seq_cod_pub INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE seq_cod_pub
+START WITH 1
+INCREMENT BY 1;
+
 /
 -- INSERINDO PUBLICAÇÕES
 INSERT INTO tb_publicacao VALUES(
@@ -300,17 +342,20 @@ INSERT INTO tb_usuario_pub VALUES (
 /
 
 -- CRIANDO SEQUÊNCIA PARA COD_OBRA
-CREATE SEQUENCE seq_cod_obra INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE seq_cod_obra
+START WITH 1
+INCREMENT BY 1;
+
 /
 -- INSERINDO OBRAS
 INSERT INTO tb_obra VALUES(
     tp_obra(
-        cod_obra.NEXTVAL,
+        seq_cod_obra.NEXTVAL,
         'it ends with us',
-        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email = 'mlmn3@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 1),
+        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email.email = 'mlmn3@cin.ufpe.br' AND E.cod_pub.cod_pub = 1),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 1),
         tp_nt_autores(
-            tp_autor('Collen Hoover')
+            tp_autores('Collen Hoover')
         ),
         va_tp_generos(tp_generos('Romance'), tp_generos('Drama'))
     )
@@ -318,12 +363,12 @@ INSERT INTO tb_obra VALUES(
 /
 INSERT INTO tb_obra VALUES(
     tp_obra(
-        cod_obra.NEXTVAL,
+        seq_cod_obra.NEXTVAL,
         'Percy Jackson e o Ladrão de Raios',
-        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email = 'rick.riordan@gmail.com'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 2),
+        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email.email = 'rick.riordan@gmail.com' AND E.cod_pub.cod_pub = 2),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 2),
         tp_nt_autores(
-            tp_autor('Rick Riordan')
+            tp_autores('Rick Riordan')
         ),
         va_tp_generos(tp_generos('Aventura'), tp_generos('Ação'), tp_generos('Fantasia'))
     )
@@ -331,12 +376,12 @@ INSERT INTO tb_obra VALUES(
 /
 INSERT INTO tb_obra VALUES(
     tp_obra(
-        cod_obra.NEXTVAL,
+        seq_cod_obra.NEXTVAL,
         'Moby Dick',
-        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email = 'jdsilva@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 5),
+        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email.email = 'jdsilva@cin.ufpe.br' AND E.cod_pub.cod_pub = 5),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 5),
         tp_nt_autores(
-            tp_autor('Herman Melville')
+            tp_autores('Herman Melville')
         ),
         va_tp_generos(tp_generos('Drama'), tp_generos('Aventura'))
     )
@@ -344,12 +389,12 @@ INSERT INTO tb_obra VALUES(
 /
 INSERT INTO tb_obra VALUES(
     tp_obra(
-        cod_obra.NEXTVAL,
+        seq_cod_obra.NEXTVAL,
         'Jane Eyre',
-        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email = 'anamaria@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 6),
+        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email.email = 'anamaria@cin.ufpe.br' AND E.cod_pub.cod_pub = 6),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 6),
         tp_nt_autores(
-            tp_autor('Charlotte Brontë')
+            tp_autores('Charlotte Brontë')
         ),
         va_tp_generos(tp_generos('Romance'), tp_generos('Gótico'))
     )
@@ -357,12 +402,12 @@ INSERT INTO tb_obra VALUES(
 /
 INSERT INTO tb_obra VALUES(
     tp_obra(
-        cod_obra.NEXTVAL,
+        seq_cod_obra.NEXTVAL,
         'O Grande Gatsby',
-        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email = 'jdsilva@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 7),
+        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email.email = 'jdsilva@cin.ufpe.br' AND E.cod_pub.cod_pub = 7),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_Pub = 7),
         tp_nt_autores(
-            tp_autor('F. Scott Fitzgerald')
+            tp_autores('F. Scott Fitzgerald')
         ),
         va_tp_generos(tp_generos('Drama'))
     )
@@ -370,13 +415,13 @@ INSERT INTO tb_obra VALUES(
 /
 INSERT INTO tb_obra VALUES(
     tp_obra(
-        cod_obra.NEXTVAL,
+        seq_cod_obra.NEXTVAL,
         'O Morro dos Ventos Uivantes',
-        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email = 'anamaria@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 8),
+        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email.email = 'anamaria@cin.ufpe.br' AND E.cod_pub.cod_pub = 8),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 8),
         tp_nt_autores(
-            tp_autor('Emily Bronte'),
-            tp_autor('Pei Sherman')
+            tp_autores('Emily Bronte'),
+            tp_autores('Pei Sherman')
         ),
         va_tp_generos(tp_generos('Drama'), tp_generos('Romance'))
     )
@@ -384,13 +429,13 @@ INSERT INTO tb_obra VALUES(
 /
 INSERT INTO tb_obra VALUES(
     tp_obra(
-        cod_obra.NEXTVAL,
+        seq_cod_obra.NEXTVAL,
         'Crime e Castigo',
-        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email = 'lcosta@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 9),
+        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email.email = 'lcosta@cin.ufpe.br' AND E.cod_pub.cod_pub = 9),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 9),
         tp_nt_autores(
-            tp_autor('Fiódor Dostoiévski'),
-            tp_autor('Guilherme')
+            tp_autores('Fiódor Dostoiévski'),
+            tp_autores('Guilherme')
         ),
         va_tp_generos(tp_generos('Ficção Policial'))
     )
@@ -398,12 +443,12 @@ INSERT INTO tb_obra VALUES(
 /
 INSERT INTO tb_obra VALUES(
     tp_obra(
-        cod_obra.NEXTVAL,
+        seq_cod_obra.NEXTVAL,
         'Morte no Nilo',
-        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email = 'mlmn3@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 10),
+        (SELECT REF(E) FROM tb_usuario_pub E WHERE E.email.email = 'mlmn3@cin.ufpe.br' AND E.cod_pub.cod_pub = 10),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 10),
         tp_nt_autores(
-            tp_autor('Agatha Christie'),
+            tp_autores('Agatha Christie')
         ),
         va_tp_generos(tp_generos('Suspense'))
     )
@@ -413,150 +458,153 @@ INSERT INTO tb_obra VALUES(
 -- INSERINDO NA TABELA SINOPSES
 INSERT INTO tb_sinopses VALUES(
     tp_sinopses(
-        'Uma história tocante sobre amor, superação e os desafios de romper ciclos abusivos e tóxicos nas relações pessoais.',
-        1
+		    (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 1),
+        'Uma história tocante sobre amor, superação e os desafios de romper ciclos abusivos e tóxicos nas relações pessoais.'
     )
 );
 /
 
 INSERT INTO tb_sinopses VALUES(
     tp_sinopses(
-        'Percy descobre ser um semideus e embarca em uma aventura épica para recuperar o raio mestre de Zeus, enfrentando criaturas mitológicas e deuses poderosos.',
-        2
+		    (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 2),
+        'Percy descobre ser um semideus e embarca em uma aventura épica para recuperar o raio mestre de Zeus, enfrentando criaturas mitológicas e deuses poderosos.'
     )
 );
 /
 
 INSERT INTO tb_sinopses VALUES(
     tp_sinopses(
-        'O capitão Ahab parte em uma obsessiva caçada pela baleia branca Moby Dick, levando sua tripulação a uma jornada trágica e filosófica sobre vingança e destino.',
-        3
+		    (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 3),
+        'O capitão Ahab parte em uma obsessiva caçada pela baleia branca Moby Dick, levando sua tripulação a uma jornada trágica e filosófica sobre vingança e destino.'
     )
 );
 /
 
 INSERT INTO tb_sinopses VALUES(
     tp_sinopses(
-        'Jane, uma jovem órfã, enfrenta dificuldades e preconceitos para construir sua própria identidade e encontrar o amor, enquanto guarda segredos sombrios em sua vida.',
-        4
+		    (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 4),
+        'Jane, uma jovem órfã, enfrenta dificuldades e preconceitos para construir sua própria identidade e encontrar o amor, enquanto guarda segredos sombrios em sua vida.'
     )
 );
 /
 
 INSERT INTO tb_sinopses VALUES(
     tp_sinopses(
-        'Um retrato do sonho americano na década de 1920, centrado em Jay Gatsby e sua obsessão por Daisy Buchanan em meio ao luxo e tragédias da alta sociedade.',
-        5
+		    (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 5),
+        'Um retrato do sonho americano na década de 1920, centrado em Jay Gatsby e sua obsessão por Daisy Buchanan em meio ao luxo e tragédias da alta sociedade.'
     )
 );
 /
 
 INSERT INTO tb_sinopses VALUES(
     tp_sinopses(
-        'Uma história de paixão intensa e vingança que acompanha a complexa relação entre Heathcliff e Catherine, marcada por traições, ódio e amor em um cenário sombrio.',
-        6
+		    (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 6),
+        'Uma história de paixão intensa e vingança que acompanha a complexa relação entre Heathcliff e Catherine, marcada por traições, ódio e amor em um cenário sombrio.'
     )
 );
 /
 
 INSERT INTO tb_sinopses VALUES(
     tp_sinopses(
-        'Raskólnikov, um jovem estudante, luta com as consequências morais e psicológicas de cometer um assassinato, explorando temas de culpa, redenção e justiça.',
-        7
+		    (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 7),
+        'Raskólnikov, um jovem estudante, luta com as consequências morais e psicológicas de cometer um assassinato, explorando temas de culpa, redenção e justiça.'
     )
 );
 /
 
 INSERT INTO tb_sinopses VALUES(
     tp_sinopses(
-        'Hercule Poirot investiga um misterioso assassinato a bordo de um cruzeiro pelo rio Nilo, onde cada passageiro parece ter motivos para matar a vítima',
-        8
+		    (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 8),
+        'Hercule Poirot investiga um misterioso assassinato a bordo de um cruzeiro pelo rio Nilo, onde cada passageiro parece ter motivos para matar a vítima'
     )
 );
 /
 
 -- INSERINDO NA TABELA REFERENCIAR
 INSERT INTO tb_referenciar VALUES(
-    tb_referenciar(
+    tp_referenciar(
         (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 1),
-        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email = 'mlmn3@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 1)
+        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email.email = 'mlmn3@cin.ufpe.br' AND U.cod_pub.cod_pub = 1),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 1)
     )
 );
 /
 
 INSERT INTO tb_referenciar VALUES(
-    tb_referenciar(
+    tp_referenciar(
         (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 2),
-        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email = 'rick.riordan@gmail.com'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 2)
+        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email.email = 'rick.riordan@gmail.com' AND U.cod_pub.cod_pub = 2),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 2)
     )
 );
 /
 
 INSERT INTO tb_referenciar VALUES(
-    tb_referenciar(
+    tp_referenciar(
         (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 3),
-        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email = 'jdsilva@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 3)
+        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email.email = 'jdsilva@cin.ufpe.br' AND U.cod_pub.cod_pub = 3),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 3)
     )
 );
 /
 
 INSERT INTO tb_referenciar VALUES(
-    tb_referenciar(
+    tp_referenciar(
         (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 4),
-        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email = 'anamaria@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 6)
+        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email.email = 'anamaria@cin.ufpe.br' AND U.cod_pub.cod_pub = 6),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 6)
     )
 );
 /
 
 INSERT INTO tb_referenciar VALUES(
-    tb_referenciar(
+    tp_referenciar(
         (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 5),
-        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email = 'jdsilva@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 7)
+        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email.email = 'jdsilva@cin.ufpe.br' AND U.cod_pub.cod_pub = 7),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 7)
     )
 );
 /
 
 INSERT INTO tb_referenciar VALUES(
-    tb_referenciar(
-        (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 6),
-        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email = 'anamaria@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 8)
+    tp_referenciar(
+        (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 6), 
+        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email.email = 'anamaria@cin.ufpe.br' AND U.cod_pub.cod_pub = 8),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 8)
     )
 );
 /
 
 INSERT INTO tb_referenciar VALUES(
-    tb_referenciar(
+    tp_referenciar(
         (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 7),
-        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email = 'lcosta@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 9)
+        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email.email = 'lcosta@cin.ufpe.br' AND U.cod_pub.cod_pub = 9),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 9)
     )
 );
 /
 
 INSERT INTO tb_referenciar VALUES(
-    tb_referenciar(
+    tp_referenciar(
         (SELECT REF(O) FROM tb_obra O WHERE O.cod_obra = 8),
-        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email = 'mlmn3@cin.ufpe.br'),
-        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub = 10)
+        (SELECT REF(U) FROM tb_usuario_pub U WHERE U.email.email = 'mlmn3@cin.ufpe.br' AND U.cod_pub.cod_pub = 10),
+        (SELECT REF(P) FROM tb_usuario_pub P WHERE P.cod_pub.cod_pub = 10)
     )
 );
 /
 
 
 -- CRIANDO SEQUÊNCIA PARA COD_COM
-CREATE SEQUENCE seq_cod_com INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE seq_cod_com
+START WITH 1
+INCREMENT BY 1;
 /
+
 -- INSERINDO NA TABELA COMENTARIO
 -- D É PARA O DONO DA PUBLICAÇÃO, P É PARA A PUBLICAÇÃO
 INSERT INTO tb_comentario VALUES(
     tp_comentario(
-        (SELECT REF(D) FROM tb_publicacao D WHERE D.email = 'mlmn3@cin.ufpe.br'), 
+        (SELECT REF(D) FROM tb_publicacao D WHERE D.email.email = 'mlmn3@cin.ufpe.br' AND D.cod_pub = 1), 
         (SELECT REF(P) FROM tb_publicacao P WHERE P.cod_pub = 1),
         seq_cod_com.NEXTVAL,
         'Também não gostei!'
@@ -566,7 +614,7 @@ INSERT INTO tb_comentario VALUES(
 
 INSERT INTO tb_comentario VALUES(
     tp_comentario(
-        (SELECT REF(D) FROM tb_publicacao D WHERE D.email = 'anamaria@cin.ufpe.br'), 
+        (SELECT REF(D) FROM tb_publicacao D WHERE D.email.email = 'anamaria@cin.ufpe.br' AND D.cod_pub = 1), 
         (SELECT REF(P) FROM tb_publicacao P WHERE P.cod_pub = 8),
         seq_cod_com.NEXTVAL,
         'Sim, já li! Achei intenso e cheio de emoções, com personagens complexos e uma atmosfera sombria.'
@@ -576,7 +624,7 @@ INSERT INTO tb_comentario VALUES(
 
 INSERT INTO tb_comentario VALUES(
     tp_comentario(
-        (SELECT REF(D) FROM tb_publicacao D WHERE D.email = 'lcosta@cin.ufpe.br'), 
+        (SELECT REF(D) FROM tb_publicacao D WHERE D.email.email = 'lcosta@cin.ufpe.br' AND D.cod_pub = 1), 
         (SELECT REF(P) FROM tb_publicacao P WHERE P.cod_pub = 9),
         seq_cod_com.NEXTVAL,
         'Estou doido para ler esse, mas ainda não tive tempo'
@@ -613,7 +661,8 @@ INSERT INTO tb_comentar VALUES(
 );
 /
 
--- INSERINDO NA TABELA SEGUIR (S é o seguidor e F é o seguido)
+-- INSERINDO NA TABELA SEGUIR
+-- S É PARA O SEGUIDOR, F É PARA O SEGUIDO
 INSERT INTO tb_seguir VALUES(
     tp_seguir(
         (SELECT REF(S) FROM tb_usuario S WHERE S.email = 'mlmn3@cin.ufpe.br'), 
@@ -698,12 +747,4 @@ INSERT INTO tb_curtir VALUES(
         (SELECT REF(A) FROM tb_publicacao A WHERE A.cod_pub = 9)
     )
 );
-/
-
--- VALUE
-UPDATE TABLE(
-    SELECT u FROM tb_usuario u WHERE u.nome = 'Rebeca Menezes'
-) e
-SET VALUE(e.va_tp_telefones) = tp_telefones('81922122431')
-WHERE e.nome = 'Rebeca Menezes';
 /
