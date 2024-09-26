3283492111fce45c1b2f075663c5a60849addd7e
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-09-25 23:54:57 -0300

    Update consultas_or.sql

--- AV6/consultas_or.sql
+++ AV6/consultas_or.sql
@@ -8,15 +8,15 @@ WHERE LENGTH(U.email) > 20;
 SELECT A.nome_completo.nome AS nome, A.nome_completo.sobrenome AS sobrenome
 FROM tb_autor A
 WHERE CARDINALITY(A.lista_premios) = (
-	SELECT MAX(CARDINALITY(B.lista_premios))
-	FROM tb_autor B
+    SELECT MAX(CARDINALITY(B.lista_premios))
+    FROM tb_autor B
 );
 
 -- Seleciona as publicações feitas em março de 2024 e exibe todos os atributos associados a essas publicações
 SELECT DEREF(P.email).email AS email, P.cod_pub, P.conteudo, P.data_pub
 FROM tb_publicacao P
 WHERE P.data_pub >= TO_DATE('2024-03-01', 'YYYY-MM-DD')
-	AND P.data_pub < TO_DATE('2024-04-01', 'YYYY-MM-DD');
+    AND P.data_pub < TO_DATE('2024-04-01', 'YYYY-MM-DD');
 
 -- Seleciona os usuários que realizaram publicações e exibe seus nomes em ordem alfabética, eliminando resultados duplicados
 SELECT U.nome_completo.nome AS nome, COUNT(DISTINCT UP.cod_pub) AS quantidade_publicacoes
@@ -40,10 +40,11 @@ WHERE T.quantidade_publicacoes = ANY(SELECT MAX(COUNT(DISTINCT UP2.cod_pub))
 -- É uma consulta à varray
 SELECT O.titulo, G.genero
 FROM tb_obra O,
-	TABLE(O.lista_generos) G
+    TABLE(O.lista_generos) G
 ORDER BY O.titulo;
 
 -- Seleciona o tamanho médio das sinopses por gênero
+-- É uma consulta à varray
 SELECT AVG(LENGTH(sinopse)) AS tamanho_medio, genero
 FROM(
     SELECT O.titulo, G.genero, S.sinopse 
