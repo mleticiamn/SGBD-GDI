2a36f9fe5367733245fb722d779615b3df8457e7
Maria Letícia <mlmn3@cin.ufpe.br>
2024-10-01 22:18:27 -0300

    refactor: livros e autores

--- ProjetoMongoDB/povoamento.js
+++ ProjetoMongoDB/povoamento.js
@@ -14,10 +14,11 @@ db.livros.insertOne(
 	data_pub: Date('28/06/2005'),
 	sinopse: 'Um artefato precioso foi roubado do Monte Olimpo e Percy é o principal suspeito. Para restaurar a paz, ele e seus amigos – jovens heróis modernos – terão de fazer mais do que capturar o verdadeiro ladrão: precisam elucidar uma traição mais ameaçadora que fúria dos deuses.',
 	generos: [
-		{nome: 'ação'},
-		{nome: 'aventura'}
+		'ação',
+		'aventura'
 		],
-	status: 'emprestado'
+	status: 'emprestado',
+	qtdd_disponivel: 3
 	}
 );
 
@@ -27,11 +28,13 @@ db.autores.insertOne(
 	nome: 'Richard Russell Riordan Jr',
 	data_nasc: Date('05/06/1964'),
 	email: 'rickriordan@gmail.com',
-	livros: [
-		{titulo: 'Percy Jackson e o Ladrão de Raios', critica: 92/100}
+	livros_escritos: [
+		{titulo_livro: 1,
+			critica: 92
+		}
 		],
 	premios: [
-		{nome: 'melhor autor 2006'}
+		'melhor autor 2006'
 		]	
 	}
 );
