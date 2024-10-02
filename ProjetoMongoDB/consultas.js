c4bd606d692b42501c8b156ba1b4dca7e2a5c9b3
mleticiamn <mlmn3@cin.ufpe.br>
2024-10-02 13:05:23 -0300

    feature: consulta com count e pretty

--- ProjetoMongoDB/consultas.js
+++ ProjetoMongoDB/consultas.js
@@ -1,4 +1,10 @@
-// Selecionando os livros que receberam uma nota maior ou igual a 90 do The New York Times
+// FIND e PRETTY: retorna os membros registrados na biblioteca
+db.membros.find({}).pretty();
+
+// FIND e COUNT: retorna a quantidade de autores
+db.autores.find({}).count();
+
+// FIND e ELEMMATCH: Selecionando os livros que receberam uma nota maior ou igual a 90 do The New York Times
 db.livros.find({
     critica: {
       $elemMatch: {
@@ -6,5 +12,5 @@ db.livros.find({
         nota: { $gte: 90 }
       }
     }
-})
+});
   
